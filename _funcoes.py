038ba9955aabcc7b72bd017ca46c2faada1b4ea1
Eduardo Will <eduwilleeg@gmail.com>
2025-09-26 21:36:50 -0300

    creating registrar_entrada

--- _funcoes.py
+++ _funcoes.py
@@ -1,13 +1,20 @@
 from datetime import datetime
 import pytz
+from _classes import Veiculo, Carro, Moto, Estadia
+import _dados
 
 # Funções
 
 def registrar_entrada(clientes):
     try:
+        # Atributo hora/data
         fuso_brasilia = pytz.timezone("America/Sao_Paulo")
-        placa = str(input("Digite a placa do veículo: ")) #id do cliente
         hora = datetime.now(fuso_brasilia)
+
+        # Definindo os atributos da classe Veiculo
+        placa = str(input("Digite a placa do veículo: ")) #id do cliente
+        modelo = str(input("Qual o modelo do veículo? ")).strip()
+        cor = str(input("Qual cor? ")).strip()
 
         # Definindo os atributos da subclasse
         tipo_veiculo = str(input("Qual o tipo do veículo? [Carro/Moto] ")).strip().capitalize()
@@ -24,15 +31,52 @@ def registrar_entrada(clientes):
             else:
                 print("Digite S ou N")
 
-        for clientesExistente in clientes:
+        # Definindo a vaga
+        if tipo_veiculo == "Carro":
+            if tam_carro == "M":
+                for vagaPretendida in _dados.vagas:
+                    if vagaPretendida[0] == "M":
+                        vaga = vagaPretendida
+                        _dados.vagas.remove(vagaPretendida)
+            elif tam_carro == "G":
+                for vagaPretendida in _dados.vagas:
+                    if vagaPretendida[0] == "G":
+                        vaga = vagaPretendida
+                        _dados.vagas.remove(vagaPretendida)
+
+        elif tipo_veiculo == "Moto":
+            if eletrica == True:
+                for vagaPretendida in _dados.vagas:
+                    if vagaPretendida[0] == "E":
+                        vaga = vagaPretendida
+                        _dados.vagas.remove(vagaPretendida)
+
+            elif eletrica == False:
+                for vagaPretendida in _dados.vagas:
+                    if vagaPretendida[0] == "C":
+                        vaga = vagaPretendida
+                        _dados.vagas.remove(vagaPretendida)
+                
+        # Verifico se o cliente ja existe na lista de clientes.
+        for clientesExistente in _dados.clientes:
             # se já existir o cliente, registra a entrada
             if placa == clientesExistente.placa:
                 print("Cadastro já existente, fazendo o registro da entrada...")
-                if tipo_veiculo == "Carro":
-                    objeto = 
-
+                estadiaNova = Estadia(placa, modelo, hora)
+                _dados.estadias.append(estadiaNova)
                 return
-            
+        
+        # Caso não exista, vamos registrar o cliente e em seguida registrar a entrada.
+        if tipo_veiculo == "Carro":
+            clienteNovo = Carro(placa, modelo, cor, vaga, tam_carro)
+            _dados.clientes.append(clienteNovo)
+        elif tipo_veiculo == "Moto":
+            clienteNovo = Moto(placa, modelo, cor, vaga, eletrica)
+            _dados.clientes.append(clienteNovo)
+
+        estadiaNova = Estadia(placa, modelo, hora)
+        _dados.estadias.append(estadiaNova)
+        return
 
 
     except TypeError as e:
